b3f5974375fe1968073d204b97f5ff25ecd2261b
yGaR <ygar@rambler.ru>
2022-07-19 16:05:51 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface ICalc<T>
+    {
+        public T Sum(T a, T b);
+        public T Div(T a, T b);
+    }
+}
